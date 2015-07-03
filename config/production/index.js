b3fdbf66961d5e8ae1acd93f6a4d3b02de73210f
西村　祐樹 <nishimura_yuki@yuki.home>
2015-07-03 18:08:30 +1000

    front page build and database setting()

--- config/production/index.js
+++ config/production/index.js
@@ -1,7 +1,8 @@
-var app = require('express');
-
-module.exports.load = function(){
-    app.set('mysql_connection', {
-    });
-
+module.exports = {
+    mysql_connection: {
+        host: 'us-cdbr-iron-east-02.cleardb.net',
+        user: 'b77c302d8a10e6',
+        password: '8ad2d3f4',
+        database: 'heroku_2c2c97ab72101ed'       
+    }
 };
